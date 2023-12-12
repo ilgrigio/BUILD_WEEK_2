adedb491294cbdbac3024dc9515ecd593271e65a
Ginevra Testa <ginevra99testaa@gmail.com>
2023-12-12 17:35:16 +0100

    footer-end

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function checkWindowWidth(){
-   let windowSize = window.innerWidth;
-   let showMore = document.querySelector("#showMore");
+function checkWindowWidth() {
+  let windowSize = window.innerWidth;
+  let showMore = document.querySelector("#showMore");
 
-   if(windowSize <= 767){
-      showMore.innerHTML = `<p class="fw-bold mb-0">Mostra altro <ion-icon name="chevron-down-outline"></ion-icon></p>`
-   }
+  if (windowSize <= 767) {
+    showMore.innerHTML = `<p class="fw-bold mb-0">Mostra altro <ion-icon name="chevron-down-outline"></ion-icon></p>`;
+  } else {
+    showMore.innerHTML = `<p class="fw-bold mb-0">Chia</p>
+    <span class="gray">Case vacanze</span>`;
+  }
 }
 
 window.addEventListener("resize", checkWindowWidth);
